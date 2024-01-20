0a33f65a60cde72fc5e5742db4882c7d71af7eec
Mahrang Saeed <79680735+mahrang@users.noreply.github.com>
2024-01-19 20:39:55 -0800

    Update renderer.h

--- src/renderer.h
+++ src/renderer.h
@@ -4,14 +4,17 @@
 #include <vector>
 #include "SDL.h"
 #include "snake.h"
+#include "gameObject.h"
 
-class Renderer {
+class Renderer : public GameObject
+{
  public:
   Renderer(const std::size_t screen_width, const std::size_t screen_height,
            const std::size_t grid_width, const std::size_t grid_height);
   ~Renderer();
 
-  void Render(Snake const &snake, SDL_Point const &food, SDL_Point const &poison);
+  void RunThread(std::shared_ptr<Snake> const snake, SDL_Point const &food, SDL_Point const &poison);
+  void Render(std::shared_ptr<Snake> const snake, SDL_Point const &food, SDL_Point const &poison);
   void UpdateWindowTitle(int score, int fps);
 
  private:
