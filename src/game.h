aa3ebfcf49cf817987499dbb52805ede6dbd2f78
Mahrang Saeed <79680735+mahrang@users.noreply.github.com>
2024-01-19 20:40:19 -0800

    Update game.h

--- src/game.h
+++ src/game.h
@@ -7,19 +7,21 @@
 #include "renderer.h"
 #include "snake.h"
 #include "item.h"
+#include "gameObject.h"
 
-class Game {
+class Game : public GameObject
+{
  public:
   Game(std::size_t grid_width, std::size_t grid_height);
-  void Run(Controller const &controller, Renderer &renderer,
+  void Run(Controller &controller, Renderer &renderer,
            std::size_t target_frame_duration);
+  void RunThread();
   int GetScore() const;
   int GetSize() const;
-  
 
  private:
-  Snake snake;
-  Item item_;
+  std::shared_ptr<Snake> snake;
+  Item _item;
   SDL_Point food, poison;
 
   int score{0};
